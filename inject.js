c8cb7456145902eb4718245b9e72662f576d236f
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2023-01-20 04:22:14 +0100

    fix agent scanner

--- inject.js
+++ inject.js
@@ -1,25 +1,25 @@
 function _(agentDirectory) {
     const fs = require('fs')
     const path = require('path')
-
+    
     function getAgents() {
         return fs.readdirSync(agentDirectory, {withFileTypes: true})
-            .filter(f => f.isFile() && f.name.endsWith('.go'))
+            .filter(f => f.isFile() && f.name.endsWith('.jar'))
             .map(f => path.join(agentDirectory, f.name))
     }
 
     const cp = require('child_process'), originalSpawn = cp.spawn
 
     cp.spawn = function (cmd, args, opts) {
-        args = args.filter(e => e !== "-XX:+DisableAttachMechanism");
+        args = args.filter(e => e !== '-XX:+DisableAttachMechanism');
 
-        delete opts.env["_JAVA_OPTIONS"];
-        delete opts.env["JAVA_TOOL_OPTIONS"];
-        delete opts.env["JDK_JAVA_OPTIONS"];
+        delete opts.env['_JAVA_OPTIONS'];
+        delete opts.env['JAVA_TOOL_OPTIONS'];
+        delete opts.env['JDK_JAVA_OPTIONS'];
 
         return originalSpawn(
             cmd,
-            [...getAgents().map(a => "-javaagent:" + a), ...args],
+            [...getAgents().map(a => '-javaagent:' + a), ...args],
             opts
         );
     }
